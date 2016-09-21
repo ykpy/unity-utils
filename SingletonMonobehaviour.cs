319dc97b3c9d237431170503dab05667b8eb24ce
ykpy <rm399343@gmail.com>
2016-09-21 23:48:40 +0900

    add SingletonMonoBehaviour.cs

---
+++ SingletonMonobehaviour.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+public class SingletonMonobehaviour<T> : MonoBehaviour where T : MonoBehaviour {
+
+	static T instance;
+	public static T Instance {
+		get {
+			return instance ?? (instance = FindObjectOfType<T>());
+		}
+	}
+
+	protected virtual void Awake() {
+		if (instance == null) {
+			instance = FindObjectOfType<T>();
+		} else if (instance != this) {
+			Destroy(this);
+		}
+	}
+
+	protected bool IsExistsInstance() {
+		return FindObjectOfType<T>() != null;
+	}
+
+}
